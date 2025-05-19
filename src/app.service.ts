d13dc1107c3cd8dae73c4d1314134af6564c361e
AliSabet81 <sabet.farshid1381@gmail.com>
2025-05-19 11:23:09 +0330

    env config

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
   getHello(): string {
+    const envTest = this.configService.get<string>('environment');
+    console.log(envTest);
     return 'Hello World!';
   }
 }
