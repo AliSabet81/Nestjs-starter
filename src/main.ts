56f6a79bb8583b481f18b95b42f967b026ccc2a3
AliSabet81 <sabet.farshid1381@gmail.com>
2025-05-21 12:13:15 +0330

    add deploy workflow

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const port = process.env.PORT ?? 3000;
   const host = process.env.HOST ?? '0.0.0.0';
+
   await app.listen(port, host);
 }
 bootstrap();
